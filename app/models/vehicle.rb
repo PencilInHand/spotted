64da92692c064d36bd892e6246562a7108156c16
David Yurek <david.a.yurek@gmail.com>
2015-02-16 01:29:52 -0500

    has a time card

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,7 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :user
   has_many :modifications
+  has_one :time_card
 
   validates :user, presence: true
   validates :vin, presence: true, uniqueness: true
