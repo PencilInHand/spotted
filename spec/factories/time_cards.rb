391dda5d8d60814ae3332fd5f76edb1c8fd4a460
David Yurek <david.a.yurek@gmail.com>
2015-02-16 01:30:28 -0500

    testing suite for time cards

---
+++ spec/factories/time_cards.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :time_card do
+    reaction_time "2015-02-16 01:16:02"
+sixty_foot_time "2015-02-16 01:16:02"
+three_thirty_foot_time "2015-02-16 01:16:02"
+eighth_mile_time "2015-02-16 01:16:02"
+eighth_mile_speed "MyString"
+thousand_foot_time "2015-02-16 01:16:02"
+fourth_mile_time "2015-02-16 01:16:02"
+fourth_mile_speed "MyString"
+  end
+
+end
