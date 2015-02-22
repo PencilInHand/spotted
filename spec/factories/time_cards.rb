c48e73382131d17cd066397212d5b3c7c3d36502
David Yurek <david.a.yurek@gmail.com>
2015-02-22 09:10:51 -0500

    time card spacing

--- spec/factories/time_cards.rb
+++ spec/factories/time_cards.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do
   factory :time_card do
     reaction_time "2015-02-16 01:16:02"
-sixty_foot_time "2015-02-16 01:16:02"
-three_thirty_foot_time "2015-02-16 01:16:02"
-eighth_mile_time "2015-02-16 01:16:02"
-eighth_mile_speed "MyString"
-thousand_foot_time "2015-02-16 01:16:02"
-fourth_mile_time "2015-02-16 01:16:02"
-fourth_mile_speed "MyString"
+    sixty_foot_time "2015-02-16 01:16:02"
+    three_thirty_foot_time "2015-02-16 01:16:02"
+    eighth_mile_time "2015-02-16 01:16:02"
+    eighth_mile_speed "MyString"
+    thousand_foot_time "2015-02-16 01:16:02"
+    fourth_mile_time "2015-02-16 01:16:02"
+    fourth_mile_speed "MyString"
   end
-
 end
