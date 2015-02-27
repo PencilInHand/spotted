d4549d357192d3f2e12c37f6e6d1c99cf94bc0dd
David Yurek <david.a.yurek@gmail.com>
2015-02-26 22:14:27 -0500

    adding in routes and default root routes for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  root 'welcome#index'
 
   resources :users do
     resources :vehicles
+    collection do
+      get :me
+    end
+  end
+
+  authenticated :user do
+    devise_scope :user do
+      root to: 'users#me'
+    end
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root 'welcome#index', as: :unauthenticated
+    end
   end
 end
