83eb599c45b4b41bdbfed9a087e77f6185ff6129
David Yurek <david.a.yurek@gmail.com>
2015-03-03 17:24:28 -0500

    clicking on the page now hides the side-bar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#side-bar-toggle').click(function() {
-    console.log($('.side-bar').css('left'));
     if ($('.side-bar').css('left') === '-200px') {
       $('.side-bar').animate({'left': '0px'}, 200);
       $('.page-wrapper').animate({'left': '200px'}, 200);
@@ -10,8 +9,10 @@ $(document).ready(function() {
     }
   });
 
-  // $('.page-wrapper').click(function() {
-  //   $('.side-bar').animate({'left': '-300px'}, 200);
-  //   $('.page-wrapper').animate({'left': '0px'}, 200);
-  // });
+  $('.page-wrapper').click(function() {
+    if ($('.side-bar').css('left') === '0px') {
+      $('.side-bar').animate({'left': '-200px'}, 200);
+      $('.page-wrapper').animate({'left': '0px'}, 200);
+    }
+  });
 });
