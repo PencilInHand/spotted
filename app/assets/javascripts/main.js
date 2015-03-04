93186741f0ae5ef3a74c628bf22eb501874d0903
David Yurek <david.a.yurek@gmail.com>
2015-03-04 08:37:14 -0500

    button needs to move with the side-bar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
-  $('#side-bar-toggle').click(function() {
-    if ($('.side-bar').css('left') === '-200px') {
-      $('.side-bar').animate({'left': '0px'}, 200);
-      $('.page-wrapper').animate({'left': '200px'}, 200);
-    } else {
-      $('.side-bar').animate({'left': '-200px'}, 200);
-      $('.page-wrapper').animate({'left': '0px'}, 200);
-    }
-  });
+	$('#side-bar-toggle').click(function() {
+		if ($('.side-bar').css('left') === '-200px') {
+			$('.side-bar').animate({'left': '0px'}, 200);
+			$('.page-wrapper').animate({'left': '200px'}, 200);
+			$('#side-bar-toggle').animate({'left': '200px'}, 200);
+		} else {
+			$('.side-bar').animate({'left': '-200px'}, 200);
+			$('.page-wrapper').animate({'left': '0px'}, 200);
+			$('#side-bar-toggle').animate({'left': '0px'}, 200);
+		}
+	});
 
-  $('.page-wrapper').click(function() {
-    if ($('.side-bar').css('left') === '0px') {
-      $('.side-bar').animate({'left': '-200px'}, 200);
-      $('.page-wrapper').animate({'left': '0px'}, 200);
-    }
-  });
+	$('.page-wrapper').click(function() {
+		if ($('.side-bar').css('left') === '0px') {
+			$('.side-bar').animate({'left': '-200px'}, 200);
+			$('.page-wrapper').animate({'left': '0px'}, 200);
+			$('#side-bar-toggle').animate({'left': '0px'}, 200);
+		}
+	});
 });
