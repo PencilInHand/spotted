982254cc503c59df359b5eb02ad8507e7af28af6
David Yurek <david.a.yurek@gmail.com>
2015-03-12 23:23:01 -0400

    added box shadow to the side-bar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 	$('#side-bar-toggle').click(function() {
-		if ($('.side-bar').css('left') === '-200px') {
+		if ($('.side-bar').css('left') === '-300px') {
 			$('.side-bar').animate({'left': '0px'}, 200);
 		} else {
-			$('.side-bar').animate({'left': '-200px'}, 200);
+			$('.side-bar').animate({'left': '-300px'}, 200);
 		}
 	});
 
 	$('.page-wrapper').click(function() {
 		if ($('.side-bar').css('left') === '0px') {
-			$('.side-bar').animate({'left': '-200px'}, 200);
+			$('.side-bar').animate({'left': '-300px'}, 200);
 		}
 	});
 
