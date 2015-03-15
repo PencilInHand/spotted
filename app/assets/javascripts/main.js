ff246f5150ac0efb2723fcdca1db9ada5bc88f70
David Yurek <david.a.yurek@gmail.com>
2015-03-15 17:58:34 -0400

    new mobile first javascripts button for sidebar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,2 +1,18 @@
 $(document).ready(function() {
+	var sb = $('.side-bar');
+	var sb_button = $('#side-bar-toggle');
+
+	sb_button.click(function() {
+		if (sb.css('right') === '-700px') {
+			sb.animate({'right': '0px'}, 200);
+		} else {
+			sb.animate({'right': '-700px'}, 200);
+		}
+	});
+
+	$('.page-wrapper').click(function() {
+		if (sb.css('right') === '0px') {
+			sb.animate({'right': '-700px'}, 200);
+		}
+	});
 });
