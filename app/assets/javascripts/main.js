7dcce8933e7344d1eab586ac32935178143e69af
David Yurek <david.a.yurek@gmail.com>
2015-03-15 21:27:37 -0400

    registration loging swap

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
-	var sb = $('.side-bar');
-	var sb_button = $('#side-bar-toggle');
 
-	sb_button.click(function() {
-		if (sb.css('right') === '-700px') {
-			sb.animate({'right': '0px'}, 200);
+  var sb = $('.side-bar');
+  var sb_button = $('#side-bar-toggle');
+	var log_reg_swap = $('#log-in-register-swap');
+	var sb_log_in = $('.side-bar-log-in');
+	var sb_register = $('.side-bar-register');
+
+  sb_button.click(function() {
+    if (sb.css('right') === '-600px') {
+      sb.animate({'right': '0px'}, 200);
+    } else {
+      sb.animate({'right': '-600px'}, 200);
+    }
+  });
+
+  $('.page-wrapper').click(function() {
+    if (sb.css('right') === '0px') {
+      sb.animate({'right': '-600px'}, 200);
+    }
+  });
+
+	log_reg_swap.click(function() {
+		if (log_reg_swap.text() === 'Not a member yet?') {
+			log_reg_swap.text('Already a member?');
+			sb_log_in.animate({'height': '0px'}, 200);
+			sb_register.animate({'height': '100%'}, 200);
 		} else {
-			sb.animate({'right': '-700px'}, 200);
-		}
-	});
-
-	$('.page-wrapper').click(function() {
-		if (sb.css('right') === '0px') {
-			sb.animate({'right': '-700px'}, 200);
+			log_reg_swap.text('Not a member yet?');
+			sb_register.animate({'height': '0px'}, 200);
+			sb_log_in.animate({'height': '100%'}, 200);
 		}
 	});
 });
