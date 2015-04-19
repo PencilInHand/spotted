ff2be646a62c801b419b98336e98be7986c71026
David Yurek <david.a.yurek@gmail.com>
2015-04-18 21:04:57 -0400

    method on vehicle for getting attributes initialize

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -5,4 +5,8 @@ class Vehicle < ActiveRecord::Base
 
   validates :user, presence: true
   validates :vin, uniqueness: true
+
+  def get_attributes_from_vin
+    # TODO(millidavids): Either here, or via javascript, get the attributes
+  end
 end
