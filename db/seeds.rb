cc8f3d959c91651568b61bedc0e3b9778b7b5cbe
David Yurek <david.a.yurek@gmail.com>
2015-05-29 20:06:10 -0400

    start seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-User.create!(name: 'David', email: 'david@example.com', password: 'password', password_confirmation: 'password')
+david = User.create!(name: 'David', email: 'david@example.com', password: 'password', password_confirmation: 'password')
+
+davids_camaro = david.vehicles.create!(vin: 'fsdljk')
