d0e1f05eff3bab53e8297b14cc15f49b9599fb79
David Yurek <david.a.yurek@gmail.com>
2015-05-30 23:48:23 -0400

    add time card to davids camaro

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 david = User.create!(name: 'David', email: 'david@example.com', password: 'password', password_confirmation: 'password')
 
 davids_camaro = david.vehicles.create!(vin: 'fsdljk')
+davids_camaro.time_cards.create!()
