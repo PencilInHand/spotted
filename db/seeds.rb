e4337541b2195baa0efe64e44512d1ee3c479e1d
David Yurek <david.a.yurek@gmail.com>
2015-05-31 00:46:08 -0400

    seed in davids car for vin api testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
+puts '...adding users...'
 david = User.create!(name: 'David', email: 'david@example.com', password: 'password', password_confirmation: 'password')
 
-davids_camaro = david.vehicles.create!(vin: 'fsdljk')
-davids_camaro.time_cards.create!()
+puts '...adding vehicles...'
+davids_camaro = david.vehicles.create!(vin: '2G1FB1E35C9166756')
