d34098ce43f4cfe7e03753b0425ef1b0b6176aaf
David Yurek <david.a.yurek@gmail.com>
2015-06-06 09:47:21 -0400

    add caption field to images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531040012) do
+ActiveRecord::Schema.define(version: 20150606134238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150531040012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "url"
+    t.string   "caption"
   end
 
   create_table "modifications", force: :cascade do |t|
