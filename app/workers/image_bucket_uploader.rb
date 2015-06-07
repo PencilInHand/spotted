eebf0da906509e564c9cef4a504409caa72631ce
David Yurek <david.a.yurek@gmail.com>
2015-06-07 00:51:49 -0500

    aws initialize now configuring aws, begin work on the image uploader

--- app/workers/image_bucket_uploader.rb
+++ app/workers/image_bucket_uploader.rb
@@ -2,7 +2,17 @@ class ImageBucketUploader
   include Sidekiq::Worker
 
   def perform(image_id)
-    s3 = Aws::S3::Client.new
-    # actually upload to s3
+    image = Image.find_by_id(image_id)
+    s3 = Aws::S3::Resource.new
+    case not nil
+    when image.vehicle
+      image_parent = "vehicle_#{image.vehicle.id}"
+    when image.modification
+      image_parent = "modification_#{image.modification.id}"
+    when image.time_card
+      image_parent = "time_card_#{image.time_card.id}"
+    else
+      raise ActiveRecord::RecordInvalid
+    end
   end
 end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,5 +3,3 @@ Aws.config.update({
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],
                                     ENV['AWS_SECRET_ACCESS_KEY']),
 })
-
-S3_BUCKET = Aws::S3::Client.new.buckets[ENV['S3_BUCKET']]
