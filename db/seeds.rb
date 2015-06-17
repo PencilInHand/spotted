c28717148171b7a5cc3582cc8ba5855a7af64332
David Yurek <david.a.yurek@gmail.com>
2015-06-17 17:10:18 -0400

    add henrys information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ alex = User.create!(name: 'Alex', email: 'alex@example.com', password: 'password
                     password_confirmation: 'password')
 alisha = User.create!(name: 'Alisha', email: 'alisha@example.com', password: 'password',
                       password_confirmation: 'password')
+henry = User.create!(name: 'Henry', email: 'henry@example.com', password: 'password',
+                     password_confirmation: 'password')
 
 puts '...adding vehicles...'
 davids_camaro = david.vehicles.create!(vin: ENV['DAVIDS_CAMARO_VIN'])
@@ -12,3 +14,4 @@ alexs_speed_three = alex.vehicles.create!(vin: ENV['ALEXS_SPEED_THREE_VIN'])
 alexs_moms_highlander = alex.vehicles.create!(vin: ENV['ALEXS_MOMS_HIGHLANDER_VIN'])
 alexs_dads_silverado = alex.vehicles.create!(vin: ENV['ALEXS_DADS_SILVERADO_VIN'])
 alishas_focus = alisha.vehicles.create!(vin: ENV['ALISHAS_FOCUS_VIN'])
+henrys_altima = henry.vehicles.create!(vin: ENV['HENRYS_ALTIMA_VIN'])
