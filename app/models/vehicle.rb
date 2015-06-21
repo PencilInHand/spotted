6ea590845bdaacb34a66d8f2e3f0383c0671b774
David Yurek <david.a.yurek@gmail.com>
2015-06-21 19:36:06 -0400

    work on the gallery titlebar

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -21,6 +21,9 @@ class Vehicle < ActiveRecord::Base
       api_url = ENV['EDMUNDS_API_URL_BASE'] + ENV['EDMUNDS_FULL_DETAILS_BY_VIN_URL']
       full_url = api_url + self.vin + '?fmt=json&api_key=' + ENV['EDMUNDS_API_KEY']
       self.edmunds_json = JSON.load(open(full_url))
+      self.make = self.edmunds_json['make']['name'].capitalize
+      self.model = self.edmunds_json['model']['name'].capitalize
+      self.year = edmunds_json['years'][0]['year']
     else
       true
     end
